32cf1194f5c7dacedeafc4c086eb9a7f2ac38ae5
Nataliia Klapchuk <nataklap@gmail.com>
2022-09-14 21:34:35 +0300

    Remake using OOP

--- index.js
+++ index.js
@@ -6,57 +6,61 @@
    */
 
 // ======== OBJECTS DEFINITIONS ========
-const dog = {
-  species: 'dog',
-  name: 'Hugo',
-  gender: 'male',
-  legs: 4,
-  hands: 0,
-  saying: 'Woof! Woof!',
-};
+class Inhabitant {
+  constructor(species, name, gender, saying, legs) {
+    this.species = species;
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
+    this.legs = legs;
+  }
 
-const cat = {
-  species: 'cat',
-  name: 'Kitsya',
-  gender: 'female',
-  legs: 4,
-  hands: 0,
-  saying: 'Meow! Meow!',
-};
+  getProperties() {
+    return ['species', 'name', 'gender', 'saying', 'legs']
+      .map((property) => this[property])
+      .join('; ');
+  }
+}
 
-const woman = {
-  species: 'human',
-  name: 'Dorothy',
-  gender: 'female',
-  legs: 2,
-  hands: 2,
-  saying: 'Well...',
-};
+class Human extends Inhabitant {
+  constructor(gender, name, saying, legs = 2) {
+    super('human', name, gender, saying, legs);
+    this.hands = 2;
+  }
+  getProperties() {
+    return `${super.getProperties()}; ${this.hands}`;
+  }
+}
 
-const man = {
-  species: 'human',
-  name: 'George',
-  gender: 'male',
-  legs: 2,
-  hands: 2,
-  saying: '...Hm',
-};
+class Woman extends Human {
+  constructor(name, saying) {
+    super('female', name, saying);
+  }
+}
 
-const inhabitants = [dog, cat, woman, man];
-const inhabitantsPropertyNames = [
-  'species',
-  'name',
-  'gender',
-  'legs',
-  'hands',
-  'saying',
-];
+class Man extends Human {
+  constructor(name, saying) {
+    super('male', name, saying);
+  }
+}
 
-// ======== OUTPUT ========
-inhabitants.forEach((inhabitant) =>
-  print(
-    inhabitantsPropertyNames
-      .map((propertyName) => inhabitant[propertyName])
-      .join('; ')
-  )
-);
+class Dog extends Inhabitant {
+  constructor(name, gender, legs = 4) {
+    super('dog', name, gender, 'Woof!', legs);
+  }
+}
+
+class Cat extends Inhabitant {
+  constructor(name, gender, legs = 4) {
+    super('cat', name, gender, 'Meow!', legs);
+  }
+}
+
+// // ======== OUTPUT ========
+const dog = new Dog('Hugo', 'male');
+const cat = new Cat('Kitsya', 'female');
+const man = new Man('George', '...Hm');
+const woman = new Woman('Dorothy', 'Well...');
+
+const inhabitants = [dog, cat, man, woman];
+inhabitants.forEach((inhabitant) => print(inhabitant.getProperties()));
